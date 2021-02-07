6e445573a21471f9b9a6d6f4fb0dfd32439e9498
sefbsdfb <78157432+sefbsdfb@users.noreply.github.com>
2021-02-07 23:06:05 +0000

    Update Fifa_By_Kappers.user.js

--- downloads/Fifa_By_Kappers.user.js
+++ downloads/Fifa_By_Kappers.user.js
@@ -2,7 +2,7 @@
 // @require      http://code.jquery.com/jquery-3.5.1.min.js
 // @name         Fifa By Kappers
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Kappers
 // @match       https://www.ea.com/fifa/ultimate-team/web-app/
@@ -19,7 +19,7 @@
 
     let PlayerDataMap = new Map();
     function hightLightPlayer(){
-        $.get( "https://raw.githubusercontent.com/sefbsdfb/fielkappers/main/Replace%20-%20Sheet1%20(2).csv", function( CSVdata) {
+        $.get( "https://raw.githubusercontent.com/sefbsdfb/fielkappers/main/Replace%20-%20Sheet1%20(3).csv", function( CSVdata) {
             var lines=CSVdata.split("\n");
             var headers=lines[0].split(",");
 
@@ -38,7 +38,7 @@
                 $('.icon-transfer').on('mouseover' , function(){
                     setTimeout(function(){
                         attachFunctionToTranfer();
-                    }, 1000);
+                    }, 2000);
                 });
             }else{
                 setTimeout(function(){
@@ -56,7 +56,7 @@
             $('.ut-tile-transfer-market').on("mouseover",function(){
                 setTimeout(function(){
                     attachfunctionToSearch();
-                }, 2000);
+                }, 3000);
             });
         }
         else{
@@ -66,7 +66,11 @@
     function attachfunctionToSearch(){
         if( $('.btn-standard.call-to-action').length>0){
             console.log('Search Button is Ready to click');
-            $('.btn-standard.call-to-action').on("mouseover",function(){
+            $('.btn-standard.call-to-action').on("click",function(){
+                var t = document.createElement("style");
+            t.type = "text/css",
+            t.innerText = "\n    .SearchResults.ui-layout-left .listFUTItem {\n        height: 60px;\n    }\n    .SearchResults.ui-layout-left .listFUTItem .label {\n        font-size: 10px;\n  }\n    .SearchResults.ui-layout-left .auction {\n        margin-top: 0 !important;\n        font-size: 12px;\n        top: 4px;\n    }\n",
+            document.head.appendChild(t)
                 highlightValues();
             });
         }
@@ -97,19 +101,37 @@
             var ExcelDataSheetPrice = PlayerDataMap.get(fullNAme[fullNAme.length-1]+""+rating);
             console.log('ExcelSheetPrice',fullNAme[fullNAme.length-1]+""+rating,"==>" , ExcelDataSheetPrice);
             console.log('SitePrice',name+""+rating+"==>" , SitePlayerPrice);
+            if(ExcelDataSheetPrice!=undefined && parseInt(SitePlayerPrice)<=parseInt(ExcelDataSheetPrice*1.03)){
+                $('.name:eq('+i+')').parent().css('background-color','orange');
+            }
             if(ExcelDataSheetPrice!=undefined && parseInt(SitePlayerPrice)<=parseInt(ExcelDataSheetPrice)){
                 $('.name:eq('+i+')').parent().css('background-color','green');
             }
+            if(ExcelDataSheetPrice!=undefined && parseInt(SitePlayerPrice)<=parseInt(ExcelDataSheetPrice*0.97)){
+                $('.name:eq('+i+')').parent().css('background-color','magenta');
+            }
+            if(ExcelDataSheetPrice!=undefined && parseInt(SitePlayerPrice)<=parseInt(ExcelDataSheetPrice*0.80)){
+                $('.name:eq('+i+')').parent().css('background-color','red');
+            }
+
         }
         $('.pagination.prev').on('click' , function(e){
             e.preventDefault();
+            var t = document.createElement("style");
+            t.type = "text/css",
+            t.innerText = "\n    .SearchResults.ui-layout-left .listFUTItem {\n        height: 60px;\n    }\n    .SearchResults.ui-layout-left .listFUTItem .label {\n        font-size: 10px;\n  }\n    .SearchResults.ui-layout-left .auction {\n        margin-top: 0 !important;\n        font-size: 12px;\n        top: 4px;\n    }\n",
+            document.head.appendChild(t)
             setTimeout(function(){
                 getPlayerDataFromSite();
             }, 500);
         });
 
-        $('.pagination.next').on('click' , function(e){
+        $('.pagination.next').on('click', function(e){
             e.preventDefault();
+            var t = document.createElement("style");
+            t.type = "text/css",
+            t.innerText = "\n    .SearchResults.ui-layout-left .listFUTItem {\n        height: 60px;\n    }\n    .SearchResults.ui-layout-left .listFUTItem .label {\n        font-size: 10px;\n  }\n    .SearchResults.ui-layout-left .auction {\n        margin-top: 0 !important;\n        font-size: 12px;\n        top: 4px;\n    }\n",
+            document.head.appendChild(t)
             setTimeout(function(){
                 getPlayerDataFromSite();
             }, 500);
